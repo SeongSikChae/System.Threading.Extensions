70456c906e1a490597e730d9e88b62d8ee718bbd
ChaeSeongSik <css5831@outlook.com>
2024-09-27 15:13:38 +0900

    AsyncLock Addition

---
+++ System.Threading.Extensions/System/Threading/AsyncLock.cs
@@ -0,0 +1,44 @@
+﻿namespace System.Threading
+{
+	/// <summary>
+	/// async-await 에 대한 임계 영역을 을 제공합니다.
+	/// </summary>
+	public sealed class AsyncLock : IDisposable
+	{
+		private readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+
+		/// <summary>
+		/// 특정 비동기 작업에 대한 임계 영역을 을 제공합니다.
+		/// </summary>
+		/// <param name="func"></param>
+		/// <param name="cancellationToken"></param>
+		/// <returns></returns>
+		public async Task LockAsync(Func<CancellationToken, Task> func, CancellationToken cancellationToken)
+		{
+			try
+			{
+				await semaphoreSlim.WaitAsync(cancellationToken);
+				await func(cancellationToken);
+			}
+			finally
+			{
+				semaphoreSlim.Release();
+			}
+		}
+
+		private bool disposedValue;
+
+		/// <summary>
+		///		Performs application-defined tasks associated with freeing, releasing, or resetting
+		///     unmanaged resources.
+		/// </summary>
+		public void Dispose()
+		{
+			if (!disposedValue)
+			{
+				disposedValue = true;
+				GC.SuppressFinalize(this);
+			}
+		}
+	}
+}
